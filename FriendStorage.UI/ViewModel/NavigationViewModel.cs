45c71b6736271525edf5daaa862daf8077aa644b
keyvan-alishiri <66223520+keyvan-alishiri@users.noreply.github.com>
2021-01-20 07:23:39 +0330

    MainWindows Load MainViewModel and MainViewModel  Load NavigationViewModel in OnStatup Create an instance of the mainviewmodel in the mainwindows parameter

--- FriendStorage.UI/ViewModel/NavigationViewModel.cs
+++ FriendStorage.UI/ViewModel/NavigationViewModel.cs
@@ -1,7 +1,27 @@
-﻿namespace FriendStorage.UI.ViewModel
-{
-  public class NavigationViewModel : ViewModelBase
-  {
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Security.Permissions;
+using FriendStorage.DataAccess;
+using FriendStorage.Model;
 
-  }
+namespace FriendStorage.UI.ViewModel
+{
+    public class NavigationViewModel : ViewModelBase
+    {
+
+        public NavigationViewModel()
+        {
+               Friends = new ObservableCollection<Friend>(); 
+        }   
+        public void Load()
+        {
+           var dataService = new FileDataService();
+           foreach (var friend in dataService.GetAllFriends())
+           {
+               Friends.Add(friend);
+           }
+        }
+
+        public ObservableCollection<Friend> Friends { get; private set; } 
+    }
 }
