702ffa5145d317b44d8a730678c0fb4fb1bef3b8
keyvan-alishiri <66223520+keyvan-alishiri@users.noreply.github.com>
2021-01-20 07:38:08 +0330

    creating Fact and Unit Test for testing  NavigationViewModel

--- FriendStorage.UITests/ViewModel/NavigationViewModelTests.cs
+++ FriendStorage.UITests/ViewModel/NavigationViewModelTests.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FriendStorage.UI.ViewModel;
+using Xunit;
 
 namespace FriendStorage.UITests.ViewModel
 {
    public class NavigationViewModelTests
     {
+
+        [Fact]
+        public void shouldLoadFriends()
+        {
+            var viewModel = new NavigationViewModel();
+            viewModel.Load();
+
+            Assert.Equal(5, viewModel.Friends.Count); 
+
+        }
+
+
+
+
     }
 }
